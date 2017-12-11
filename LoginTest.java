c2b1921e7b6e9b16c58badf7cc7290dac8e2ffcc
manojk2829 <34441817+manojk2829@users.noreply.github.com>
2017-12-11 17:52:20 +0530

    Profile New Branch

--- LoginTest.java
+++ LoginTest.java
@@ -5,10 +5,4 @@ Enter User Name and Password
 Login User - manojk2829@gmail.om
 Password- test@123
 
-Profile Testing  -- Click Profile Test Link.
-Test Profile Page then Logout The Facebook.
-  
-  Thanks
-
-
 Create New Facebook Profile Page

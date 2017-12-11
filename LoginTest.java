0937b89db224a917796d3f4d11c1e959032610c0
manojk2829 <34441817+manojk2829@users.noreply.github.com>
2017-12-11 17:20:06 +0530

    Remove Password and UserName

--- LoginTest.java
+++ LoginTest.java
@@ -2,8 +2,4 @@ Open Browser google chrome
 Url - http://facebook.com/
 Enter User Name and Password
 
-Enter User Name - manojk2829@gmail.com
-Password -- test@1234
-
-
 Test Profile Page then Logout The Facebook.

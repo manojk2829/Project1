fbc62afff1d300f31fa758d017996c1c6b52adbb
manojk2829 <34441817+manojk2829@users.noreply.github.com>
2017-12-11 17:45:30 +0530

    Enter UserName and Password Again

--- LoginTest.java
+++ LoginTest.java
@@ -2,4 +2,10 @@ Open Browser google chrome
 Url - http://facebook.com/
 Enter User Name and Password
 
+Login User - manojk2829@gmail.om
+Password- test@123
+
+Profile Testing  -- Click Profile Test Link.
 Test Profile Page then Logout The Facebook.
+  
+  Thanks
